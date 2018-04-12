c498f77a6a843755298d3b6ca395da04c1db2d53
lamnguyen1210 <soquadi3@gmail.com>
2018-04-12 15:35:27 +0700

    Bài thực hành

---
+++ Bai2SW/src/Program/StudentInfo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Program;
+/**
+ *
+ * @author pc
+ */
+public class StudentInfo {
+    String HoTen;
+    String NamSinh;
+    String GioiTinh;
+    //getter
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public String getNamSinh() {
+        return NamSinh;
+    }
+
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+    //setter
+    public void setHoTen(String HoTen) { this.HoTen = HoTen; }
+    public void setNamSinh(String NamSinh) { this.NamSinh = NamSinh; }
+    public void setGioiTinh(String GioiTinh) { this.GioiTinh = GioiTinh; }
+    //main
+    
+}
+
