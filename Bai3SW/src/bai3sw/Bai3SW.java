2dd565eace5e88c78e4b091a6d2a49bd1b5d2b33
lamnguyen1210 <soquadi3@gmail.com>
2018-04-18 10:03:26 +0700

    Bai Thuc hanh Java
    Bai Thuc hanh Java

---
+++ Bai3SW/src/bai3sw/Bai3SW.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai3sw;
+
+/**
+ *
+ * @author pc
+ */
+public class Bai3SW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new ListView_Bai3().setVisible(true);
+    }
+    
+}
